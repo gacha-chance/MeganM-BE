780325f11cffc74a88127bdc7b7215378475ad1a
Megan <megan.miller@smoldev.dev>
2019-07-29 14:00:37 -0500

    updating schema to include calculation tables

--- data/migrations/20190728122510_gacha-chance.js
+++ data/migrations/20190728122510_gacha-chance.js
@@ -5,8 +5,26 @@ exports.up = function(knex) {
         tbl.string('username').unique(),
         tbl.string('password')
     })
+    .createTable('chanceOfAcquiring', tbl => {
+      tbl.increments('id'),
+      tbl.decimal('chance'),
+      tbl.integer('user_id').references('id').inTable('users')
+    })
+    .createTable('compoundChance', tbl => {
+      tbl.increments('id'),
+      tbl.decimal('compoundedChance'),
+      tbl.integer('user_id').references('id').inTable('users')
+    })
+    .createTable('numberOfRolls', tbl => {
+      tbl.increments('id'),
+      tbl.integer('rollNum'),
+      tbl.integer('user_id').references('id').inTable('users')
+    })
 };
 
 exports.down = function(knex) {
   dropTableIfExists('users');
+  dropTableIfExists('numberOfRolls');
+  dropTableIfExists('CompoundChance');
+  dropTableIfExists('chanceOfAcquiring')
 };
