0af53cd90c8c4064417655b53ef053eb90c2230f
Megan <megan.miller@smoldev.dev>
2019-07-29 15:03:27 -0500

    added add x thing

--- data/dataModeling/usersModel.js
+++ data/dataModeling/usersModel.js
@@ -6,6 +6,9 @@ module.exports = {
     findBy,
     add,
     remove,
+    addChance,
+    addRollNum,
+    addCompChance
 }
 
 
@@ -37,4 +40,31 @@ function remove(id) {
     return db('users')
     .where('id', id)
     .del()
+}
+
+function addChance(chance) {
+    return db('chanceOfAcquiring')
+    .insert(chance, 'id')
+    .then(chance => {
+        const chanceID = chance[0];
+        return chanceID;
+    })
+}
+
+function addRollNum(rollNum) {
+    return db('numberOfRolls')
+    .insert(rollNum, 'id')
+    .then(rollNum => {
+        const rollID = rollNum[0];
+        return rollID;
+    })
+}
+
+function addCompChance(compChance) {
+    return db('compoundChance')
+    .insert(compChance, 'id')
+    .then(compChance => {
+        const compCID = compChance[0];
+        return compCID;
+    })
 }
