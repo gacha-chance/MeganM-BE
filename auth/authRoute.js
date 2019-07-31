79aceeee396542eb15406342a35b97c78aa67ef2
Celaira <9338502+Celaira@users.noreply.github.com>
2019-07-31 12:24:36 -0500

    testing id

--- auth/authRoute.js
+++ auth/authRoute.js
@@ -29,7 +29,7 @@ router.post('/login', (req, res) => {
         if(user && bcrypt.compareSync(password, user.password)) {
             const token = generateToken(user);
             res.status(200).json({
-                ...user,
+                id: `${user.id}`
                 message: `Welcome ${user.username}`,
                 token
             })
